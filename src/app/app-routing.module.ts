0048efb299a84345ce315e9dc022570529b50a95
Josip Paradzik <josip.paradzik@emea.merkleinc.com>
2023-06-26 13:12:56 +0200

    feature-01/Create authorization component with auth service, and header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthComponent } from './auth/auth.component';
+import { BoardComponent } from './board/board.component';
+import { AuthGuard } from './auth/auth.guard';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/board', pathMatch: 'full' },
+  { path: 'auth', component: AuthComponent },
+  { path: 'board', component: BoardComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '/board' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
